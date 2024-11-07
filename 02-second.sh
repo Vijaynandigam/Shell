c25b61ef6a2be20ab87a34c6b8e205fb3442aa60
vijay <nandigamvijay2606@gmail.com>
2024-11-07 11:11:26 +1100

    second one

---
+++ 02-second.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Dhoni
+PERSON2=Kohli
+
+echo "$PERSON1: : HI $PERSON2, How is the ptch today"
+echo "$PERSON2: : HI $PERSON1, The pitch s good"
+echo "$PERSON1: Then what are we choosing $PERSON2"
+echo "$PERSON2: Thinking of bat first, I was also tinking same $PERSON1"
