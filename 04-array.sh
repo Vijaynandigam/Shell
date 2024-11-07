4d21ecf819d23cc0a98b0e7388c2e05de850584a
vijay <nandigamvijay2606@gmail.com>
2024-11-07 12:09:18 +1100

    Third one

---
+++ 04-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("Apple, Banana, Mango")
+
+echo " First fruit is ${FRUITS[0]}"
+
+echo " The second fruit is ${FRUITS[1]}"
+
+echo "the third fruit is ${FRUITS[2]}"
+
+echo " All of them is ${FRUITS[@]}"
