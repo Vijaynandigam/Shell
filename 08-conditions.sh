da9f94da0815795064bab32af217e961e263daed
vijay <nandigamvijay2606@gmail.com>
2024-11-07 14:26:44 +1100

    Third one

---
+++ 08-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 20 ] #gt
+then 
+   echo "Given number: $NUMBER is grather than 20"
+
+else
+    echo "Given number is : $NUMBER is less than 20"
+
+fi    
