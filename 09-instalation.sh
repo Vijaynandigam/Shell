3fcf4dc2db76899f969c467aa7e981d3016dcaf1
vijay <nandigamvijay2606@gmail.com>
2024-11-07 14:50:59 +1100

    Third one

--- 09-instalation.sh
+++ 09-instalation.sh
@@ -12,5 +12,14 @@ then
 
 fi
 
-dnf install git -y
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+  echo "git is not installed and going to install it..."
+  dnf install git -y
+
+else
+  echo "Git is already installed nothing to do...."
+fi    
 
