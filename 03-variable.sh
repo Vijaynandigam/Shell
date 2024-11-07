8fea8e4280529e1ba2b7f47cb3466b102a40d1db
vijay <nandigamvijay2606@gmail.com>
2024-11-07 12:02:32 +1100

    Third one

--- 03-variable.sh
+++ 03-variable.sh
@@ -2,6 +2,12 @@
 
 echo " Please enter the username: :"
 
-read USERNAME
+read -s USERNAME
 
 echo "The username entered is : $USERNAME"
+
+echo "Please senter your password: ;"
+
+read -s PASSWORD
+
+echo "The password is : $PASSWORD"
